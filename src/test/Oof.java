79d370bd1a46c6f087a7c10d6d940966f8d2bc5a
Dom Williams <me@domwillia.ms>
2022-09-28 22:00:33 +0300

    Add test cases to play with

---
+++ src/test/Oof.java
@@ -0,0 +1,16 @@
+
+public class Oof {
+
+    int a = 100;
+
+    int nice(int i) {
+        return a + i;
+    }
+
+    public static void main(String[] args) {
+        Oof oof = new Oof();
+        int x = oof.nice(50);
+    }
+
+
+}
